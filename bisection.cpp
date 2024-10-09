7c696e8b64f763ac8f02287d8a7b15db15b0a3e1
Md Asraful Molla <armdasraful@gmail.com>
2024-10-09 12:52:52 +0600

    Added files to the repository

---
+++ bisection.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+double func(double x)
+{
+    return pow(x, 3) - x - 1;
+}
+int main()
+{
+    double a, b, c;
+    cout << "Enter two point " << endl;
+    cin >> a >> b;
+    int n = 0;
+    if (a < b)
+    {
+        double fa, fb;
+        fa = func(a);
+        fb = func(b);
+        if (fa * fb < 0)
+        {
+            cout << "n" << setw(20) << "a" << setw(20) << "f(a)" << setw(20) << "b" << setw(20) << "f(b)" << setw(20) << "c" << setw(20) << "f(c)" << endl;
+            while (1)
+            {
+
+                cout << ++n << setw(20) << a << setw(20) << func(a) << setw(20) << b << setw(20) << func(b) << setw(20);
+                c = (a + b) / 2;
+                cout << c << setw(20) << func(c) << endl;
+
+                if (func(c) == 0)
+                {
+                    cout << "The root is=" << c << endl;
+                    break;
+                }
+                else if (func(a) * func(c) < 0)
+                {
+                    b = c;
+                }
+                else if (func(c) * func(b) < 0)
+                {
+                    a = c;
+                }
+                if (abs(func(c)) <= 0.00005)
+                    break;
+            }
+            cout << "root=" << c << endl;
+        }
+        else
+        {
+            cout << "Invalid input" << endl;
+        }
+    }
+    else
+    {
+        cout << "invalid input" << endl;
+    }
+
+    return 0;
+}
---
+++ regular_falsi_methods.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+double f(double x)
+{
+    return x * x * x - x - 1;
+}
+int main()
+{
+    double x0, x1, x2;
+    cin >> x0 >> x1;
+    if (x0 < x1)
+    {
+        if (f(x0) * f(x1) < 0)
+        {
+            int n = 0;
+            cout << "n" << setw(20) << "x0" << setw(20) << "f(x0)" << setw(20) << "x1" << setw(20) << "f(x1)" << setw(20) << "x2" << setw(20) << "f(x2)"<< setw(20)<<"Update" << endl;
+            while (1)
+            {
+                x2 = x0 - (f(x0) * (x1 - x0)) / (f(x1) - f(x0));
+                if (f(x2) == 0.0 || abs(f(x2)) <= 0.00005)
+                {
+                    cout << "Root is " << x2 << endl;
+                    return 0;
+                }
+                if (f(x0) * f(x2) < 0.0)
+                {
+                    cout << ++n << setw(20) << x0 << setw(20) << f(x0) << setw(20) << x1 << setw(20) << f(x1) << setw(20) << x2 << setw(20) << f(x2)<< setw(20)<<"x1 = x2" << endl;
+                    x1 = x2;
+                }
+                else if (f(x2) * f(x1) < 0.0)
+                {
+                    cout << ++n << setw(20) << x0 << setw(20) << f(x0) << setw(20) << x1 << setw(20) << f(x1) << setw(20) << x2 << setw(20) << f(x2)<< setw(20)<<"x0 = x2" << endl;
+                    x0 = x2;
+                }
+            }
+        }
+        else
+        {
+            cout << "Invalid a and b" << endl;
+        }
+    }
+    else
+    {
+        cout << "Invalid a and b" << endl;
+    }
+}
