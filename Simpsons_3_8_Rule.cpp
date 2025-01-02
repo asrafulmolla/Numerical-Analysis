d99faca771e14a266e0fb3a83fd34f0400e6b6a2
Md. Asraful Molla <128937137+asrafulmolla@users.noreply.github.com>
2025-01-02 15:14:22 +0600

    Update Simpsons_3_8_Rule.cpp

--- Simpsons_3_8_Rule.cpp
+++ Simpsons_3_8_Rule.cpp
@@ -1,26 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Define the function to integrate
-double f(double x)
-{
-    return 1 / (1 + x); // Function 1 / (1 + x)
+// Define multiple functions
+double f1(double x) {
+    return 1 / (1 + x); // Function: 1 / (1 + x)
 }
 
-int main()
-{
+double f2(double x) {
+    return sin(x); // Function: sin(x)
+}
+
+double f3(double x) {
+    return cos(x); // Function: cos(x)
+}
+
+double f4(double x) {
+    return exp(x); // Function: e^x
+}
+
+int main() {
     int n;         // Number of subintervals (must be a multiple of 3)
     double a, b;   // Lower and upper limits of the integral
+
+    // Prompt the user to select the function
+    cout << "Select the function to integrate:\n";
+    cout << "1. 1 / (1 + x)\n";
+    cout << "2. sin(x)\n";
+    cout << "3. cos(x)\n";
+    cout << "4. e^x\n";
+    int choice;
+    cin >> choice;
+
+    // Assign the selected function
+    double (*f)(double); // Pointer to the chosen function
+    switch (choice) {
+        case 1: f = f1; break;
+        case 2: f = f2; break;
+        case 3: f = f3; break;
+        case 4: f = f4; break;
+        default:
+            cout << "Invalid choice! Please restart the program.\n";
+            return 1;
+    }
 
     // Prompt the user to enter the number of subintervals
     cout << "Enter the number of interval parts (must be a multiple of 3): ";
     cin >> n;
 
     // Validate that the number of subintervals is a multiple of 3
-    if (n % 3 != 0)
-    {
+    if (n % 3 != 0) {
         cout << "Error: Number of interval parts must be a multiple of 3. Please restart the program and try again.\n";
-        return 1; // Exit the program if the condition is not met
+        return 1;
     }
 
     // Prompt the user to enter the lower and upper limits of integration
@@ -35,14 +65,12 @@ int main()
 
     // Generate the x values based on the subinterval width
     x[0] = a;
-    for (int i = 1; i <= n; i++)
-    {
+    for (int i = 1; i <= n; i++) {
         x[i] = x[i - 1] + h;
     }
 
-    // Compute the corresponding y values using the function f(x)
-    for (int i = 0; i <= n; i++)
-    {
+    // Compute the corresponding y values using the selected function
+    for (int i = 0; i <= n; i++) {
         y[i] = f(x[i]);
     }
 
@@ -50,14 +78,10 @@ int main()
     double y3 = 0, y2 = 0;
 
     // Compute the summation terms for 3h/8 Rule
-    for (int i = 1; i < n; i++)
-    {
-        if (i % 3 == 0)
-        {
+    for (int i = 1; i < n; i++) {
+        if (i % 3 == 0) {
             y2 += y[i]; // Add y values for indices that are multiples of 3
-        }
-        else
-        {
+        } else {
             y3 += y[i]; // Add y values for other indices
         }
     }
